6b489bbda63b08be5fd6cfb362d68179ad4b9d71
avleen19 <123055235+avleen19@users.noreply.github.com>
2024-12-10 15:48:23 +0530

    Delete projectpharmacybackend directory

--- projectpharmacybackend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signUp, login } = require('../Controllers/authController');
-const router = express.Router();
-
-router.post('/sign-up', signUp);
-
-router.post('/login', login);
-
-module.exports = router;
